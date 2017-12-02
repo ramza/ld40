ef7fc1e869971a91294b428b61f5c874dde67190
ramza <jpettyart@gmail.com>
2017-12-02 18:18:33 -0500

    created prefabs and added input handler

--- cats/Assets/Cat/CatController.cs
+++ cats/Assets/Cat/CatController.cs
@@ -9,7 +9,6 @@ public class CatController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
-		
 	}
 	
 	// Update is called once per frame
---
+++ cats/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+	public GameObject spawnObject;
+	public List<GameObject> catList;
+	public Camera cam;
+	float timer = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if(timer > 2f && Input.GetMouseButton(0)){
+
+			timer = 0;
+			var screenPos = Input.mousePosition;
+			var worldPos = cam.GetComponent<Camera>().ScreenToWorldPoint(screenPos);
+		
+			Instantiate(spawnObject,new Vector3(worldPos.x, worldPos.y, 0),Quaternion.identity);
+			Debug.Log(worldPos);
+
+		}
+		
+	}
+}
